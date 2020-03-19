fdb6b9f1de620641e799eb7d8bc3a56371d5c2f4
Kwonhyukjoon <victorehko12@naver.com>
2020-03-19 19:30:45 +0900

    username 버그 수정 및 네이버아이디 로그인

--- src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,19 +8,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
 
-    private final PostsService postsService;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("usernam", user.getName());
+        }
         return "index";
     }
 
